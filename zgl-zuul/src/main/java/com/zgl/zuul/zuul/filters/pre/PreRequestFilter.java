0d6dcb82e23fc303ed8af82db70249eb21746f57
zhuguolong <zhuguolongi@outlook.com>
2018-12-21 14:43:59 +0800

    增加ZuulFilter

---
+++ zgl-zuul/src/main/java/com/zgl/zuul/zuul/filters/pre/PreRequestFilter.java
@@ -0,0 +1,90 @@
+package com.zgl.zuul.zuul.filters.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author zhugu
+ * @version 1.0
+ * 2018/12/21 11:30
+ */
+public class PreRequestFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PreRequestFilter.class);
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        Map<String, Object> paramsMap = paramsMap(request.getParameterMap());
+        log.info("请求接口：[{}][{}]，请求参数：{}", method, requestURI, paramsMap.toString());
+        return null;
+    }
+
+    /**
+     * 获取请求参数
+     * @param reqMap request.getParameterMap()
+     * @return
+     */
+    public static Map<String, Object> paramsMap(Map reqMap) {
+        Map<String, Object> returnMap = new HashMap<>();
+
+        if (reqMap != null) {
+
+            Iterator entries = reqMap.entrySet().iterator();
+
+            Map.Entry entry;
+            String name;
+            StringBuilder value;
+
+            while (entries.hasNext()) {
+                value = new StringBuilder();
+                entry = (Map.Entry) entries.next();
+                name = (String) entry.getKey();
+                Object valueObj = entry.getValue();
+                if (null == valueObj) {
+                    value = new StringBuilder();
+                } else if (valueObj instanceof String[]) {
+                    String[] values = (String[]) valueObj;
+                    for (String value1 : values) {
+                        value.append(value1).append(",");
+                    }
+                    value = new StringBuilder("[" + value.substring(0, value.length() - 1) + "]");
+
+                } else {
+                    value = new StringBuilder(valueObj.toString());
+                }
+                if (value.length() > 2000) {
+                    value = new StringBuilder("大数据已截取");
+                }
+                returnMap.put(name, value.toString());
+            }
+        }
+        return returnMap;
+    }
+}
