da323473a46067c0eeccd31d3ba4458867772b68
zhuguolong <zhuguolongi@outlook.com>
2018-12-21 15:17:58 +0800

    增加user模块

---
+++ zgl-user/src/test/java/com/zgl/user/zgluser/ZglUserApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zgl.user.zgluser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZglUserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
