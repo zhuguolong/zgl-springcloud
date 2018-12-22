30568f1fbc5d25170622cfe76592074a0d938871
zhuguolong <zhuguolongi@outlook.com>
2018-12-22 19:17:21 +0800

    网关过滤器

--- zgl-zuul/src/main/java/com/zgl/zuul/zuul/ZuulApplication.java
+++ zgl-zuul/src/main/java/com/zgl/zuul/zuul/ZuulApplication.java
@@ -1,15 +1,23 @@
 package com.zgl.zuul.zuul;
 
+import com.zgl.zuul.zuul.filters.pre.PreRequestFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class ZuulApplication {
+public class ZuulApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean
+    public PreRequestFilter preRequestFilter() {
+        return new PreRequestFilter();
     }
 
 }
