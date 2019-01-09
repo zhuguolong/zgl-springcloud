5a70c57560ee506f85b7409d3b3c123098bc86f4
zhuguolong <zhuguolongi@outlook.com>
2019-01-09 16:10:11 +0800

    添加feign

--- zgl-user/src/main/java/com/zgl/user/UserApplication.java
+++ zgl-user/src/main/java/com/zgl/user/UserApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableEurekaClient
-@SpringBootApplication
+@EnableFeignClients
 @MapperScan("com.zgl.user.domain")
+@SpringBootApplication
 public class UserApplication {
 
     public static void main(String[] args) {
